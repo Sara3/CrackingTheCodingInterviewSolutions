43e15ae5afa166b3610d30da9fa24c6b844692c1
Sara <Sabera.daqiq@yahoo.com>
2016-12-05 16:18:12 -0800

    stuff

--- 1.8(zeroMatrix).cpp
+++ 1.8(zeroMatrix).cpp
@@ -1,28 +1,69 @@
 #include <iostream>
+#include <vector>
 #include <string.h>
 using namespace std; 
-#define N 5
-int main(){
-		bool hasZero =0;
-		int mat[N][N] = {{1,2,3,4,5},{6,7,0,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
-		int* cur= &(mat[0][0]);
-		for(int i=0; i<N*N; i++){
-				if(*cur==0){
-					hasZero = true; 
-					break;
-				}
-			
-			cur++;
-		}
 
-		if(hasZero){
-			memset(mat,0, sizeof(mat[0][0])*N*N );
-		}
-
-for(int i=0; i<N; i++){
-	for(int j=0; j<N; j++){
-		cout<<mat[i][j]<<endl;
+#define N 3
+/*
+vector<vector<int> > setzero(int a[N][N], int m, int j){
+	vector<vector<int> >arr(N, vector<int>(N));
+	for(int i=0; i<N; i++){
+		arr[i].assign(a[i], a[i]+N);
 	}
-	return 0; 
+	fill(arr[m].begin(), arr[m].end(), 0);
+	for(int l=0; l<N; l++){
+		arr[l][j] = 0;
+	}
+
+	return arr; 
 }
+*/
+
+void zeroRow(int a[N][N],int row){
+	for(int i=0; i<N; i++){
+		a[row][i]=0;
+	}
+}
+void zeroCol(int a[N][N],int col){
+	for(int i=0; i<N; i++){
+		a[i][col] =0; 
+	}
+}
+
+void setzero(int a[N][N]){
+
+	bool row[N] ;
+	bool col[N] ;
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			if(a[i][j]==0) {
+				row[i] = true;
+				col[j] = true;
+			}
+		}
+	}
+	for(int l=0; l<N; l++){
+		if(row[l]) {
+			cout<<row[l]<<endl;
+			zeroRow(a, l);}
+		if(col[l]) zeroCol(a, l);
+	}
+}
+
+void display(int a[N][N]){
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			cout<<a[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+int main(){
+int a[N][N]= {{1,2,3},{6,7,0},{8,9,3}};
+
+setzero(a);
+display(a);
+
+	return 0; 
 }
