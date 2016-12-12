a8f732c7a30d9db399a9c50ebe208b0ae98d3e08
Sara <Sabera.daqiq@yahoo.com>
2016-12-12 15:30:11 -0800

    last info

--- 2.5(sumlist).cpp
+++ 2.5(sumlist).cpp
@@ -2,14 +2,9 @@
 #include <math.h> 
 #include <forward_list>
 using namespace std; 
-
-int main(){
-	forward_list<int> a; 
-	forward_list<int> b; 
+/*
+forward_list<int> addlist(forward_list<int> a, forward_list<int> b){
 	forward_list<int> c; 
-	a.assign({7,1,6});
-	b.assign({5,9,2});
-
 	int sum =0;
 	int sum2 =0;
 	int total =0;
@@ -19,7 +14,7 @@ int main(){
 		sum += i*pow(10,j);
 		j++;
 	}
-	
+
 	j=0;
 	for(int&i:b) {
 		sum2 += i*pow(10,j);
@@ -35,6 +30,102 @@ int main(){
 	for(int&i:c) {
 		cout<<i<<" ";
 	}
+	return c;
+}
+
+*/
+class linkedlist{
+public:
+	struct Node{
+		int data; 
+		Node* next; 
+	};
+
+	linkedlist(){
+		head = NULL;
+	}
+
+	void insert(int val){
+		Node *n= new Node();
+		n->data = val;
+		n->next = head; 
+		head = n; 
+	}
+
+
+	void display(){
+			Node *temp; 
+			if(head==NULL){
+				cout<<"empty";
+			}
+			temp = head;
+			while(temp!=NULL){
+				cout<<temp->data<<"   "; 
+				temp = temp->next; 
+			}	
+	}
+
+	int pop(){
+		Node *n ;
+		n=head;
+		head=head->next; 
+		return n->data;
+	}
+	
+	linkedlist add(linkedlist a, linkedlist b, int curry){
+		linkedlist c; 
+		Node *n;
+		Node *m;
+		n=a.head;
+		m=b.head;
+		int sum=0;
+		while (n!=NULL){
+			sum=n->data + m->data+curry;
+			c.insert(sum%10);
+			curry = sum/10; 
+			n=n->next;
+		}
+		c.insert(curry);
+		return c;
+
+	}
+
+	/*
+	~linkedlist(){
+		Node *temp = new Node();
+		while (head != NULL)
+		{
+			temp = head->next;
+		    delete head;
+		    head = temp;
+		}
+		delete temp;
+	}
+*/
+
+private:
+	Node* head; 
+};
+
+
+
+int main(){
+	linkedlist a; 
+	linkedlist b;
+	linkedlist res;
+	a.insert(1);
+	a.insert(2);
+	a.insert(3);
+	a.display();
+	cout<<endl;
+	b.insert(3);
+	b.insert(0);
+	b.insert(9);
+	b.display();
+	cout<<endl<<endl;
+	res=res.add(a, b, 0);
+	res.display();
+
 
 	return 0; 
 }
